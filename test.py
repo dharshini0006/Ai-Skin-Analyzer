1b8866570a953f68c24ffe39cd7e5bf54b0e0bbd
Dharshini <dharshinibala.eec16@gmail.com>
2026-06-08 14:10:24 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBSsK2QKcxYJLmGZB_1mJLJwiTfToLq0rw")
-
-model = genai.GenerativeModel("gemini-1.5-flash")
-
-response = model.generate_content("Say Hello")
-
-print(response.text)
